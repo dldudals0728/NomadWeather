b8dd538cd65a93050149e2ccdf6032eb65958755
devid <dldudals0728@naver.com>
2022-07-26 16:44:59 +0900

    #2.4 Layout System

--- App.js
+++ App.js
@@ -1,30 +1,17 @@
-import { StatusBar } from "expo-status-bar";
-// 1. View: RN에서 거이 모든 요소는 View에 넣어야 한다. 일종의 container. not div, p, span!
-import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      {/* 2. 모든 text는 꼭 <Text></Text> 컴포넌트 안에 넣어야 한다. not p, span , h1, h6! (View로 했더니 error) */}
-      <Text style={styles.text}>Hello</Text>
-      {/* 6. 일부 component는 화면에 나타나지 않고, OS와 대화하기 위한 수단으로 사용된다. (like StatusBar) */}
-      <StatusBar style="auto" />
+    // View는 기본적으로 flex box이고, flexDirection의 기본값은 "column"이다!
+    <View style={{ flex: 1, flexDirection: "row" }}>
+      {/* 
+      React Native에서는 height와 width를 가지고 layout을 제작하지 않는다. 절대!
+      React Native에서는 "숫자"만 생각하면 된다.(비율!) 
+      */}
+      <View style={{ flex: 1, backgroundColor: "tomato" }}></View>
+      <View style={{ flex: 1.5, backgroundColor: "teal" }}></View>
+      <View style={{ flex: 1, backgroundColor: "orange" }}></View>
     </View>
   );
 }
-
-// 4. SytleSheet.create 내부에 들어가는 것이 obj이기 때문에, <View style={{obj}}>처럼 바로 작성할 수 있다.
-// 5. styles = StyleSheet.create({})를 거치지 않고, styles = {container: {}}로 바로 사용이 가능하나, StyleSheet.create를 사용할 경우 자동완성기능이 제공된다.
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    // 3. 사용 가능한 style(ex. backgroundColor)이 있고, 사용이 불가능한 style(ex. border)이 있다.
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  text: {
-    fontSize: 28,
-    color: "red",
-  },
-});
--- README.md
+++ README.md
@@ -16,13 +16,15 @@ JS는 개발자들이 운영체제를 상대로 메세지를 주고 받기 위�
 
 # 2. Expo setup
 
-Expo는 앱 전체를 컴파일하지 않아도, JS코드를 바로 전송시켜 코드를 미리보기 할 수 있도록 도와준다. (Java, xcode등이 없어도 됨!)
+Expo는 앱 전체를 컴파일하지 않아도, JS코드를 바로 전송시켜 코드를 미리보기 할 수 있도록 도와준다.<br>
+(Java, xcode등이 없어도 됨!)
 [doc.expo.dev](https://docs.expo.dev/)
 
 <pre>
 Requirement
     node js(LTS)
 </pre>
+<hr />
 
 1. expo-cli 설치
 
@@ -38,11 +40,15 @@ $ brew update
 $ brew install watchman
 ```
 
+<hr />
+
 2. Expo 앱 설치
    앱스토어에서 Expo 앱을 설치 및 로그인
 
-   > in Android: Expo  
-   > in IOS: Expo Go
+> in Android: Expo  
+> in IOS: Expo Go
+
+<hr />
 
 3. initialize Expo project
 
@@ -51,6 +57,8 @@ expo init my-app
 ```
 
 > option: blank(첫번째) 선택
+
+<hr />
 
 4. app 실행
    Expo project 폴더로 이동하여 명령어 입력.
@@ -66,3 +74,25 @@ expo login
 ```
 
 > Username/Email Address & Password 입력 시 앱과 연동 가능.
+
+# 3. study
+
+[React Native](https://reactnative.dev/)
+React Native의 Component, API가 많이 줄어들은 상태이다.<br>
+React Native의 공식 문서에도 community package를 사용하라고 권고.
+
+## Component VS APIs
+
+Component: 화면에 렌더링 할 항목(View, Text, ...)<br>
+API: JavaScript code. JS코드가 운영체제와 소통하는 것(Vibration)<br>
+
+<br>
+추가적으로, ayncStorage처럼 deprecated된 기능은 커뮤니티에 의존한 package를 사요할 수 밖에 없다.<br>
+**이에 Expo에서 꼭 필요한 component, API를 개발했고, 이것이 Expo SDK이다.**
+
+```
+Recap
+1. React Native팀은 RN를 빠르게 만드는 것에 집중하기 위해 서비스 제공 부분을 축소시켰다.
+2. 따라서 community에서 필요한 package를 받아서 사용해야 하는데, 많은 package가 있고, 몇몇은 버그나 업데이트가 없는 package들도 있다.
+3. 이에 Expo에서는 우리가 필요한 package를 만들었고, 좋은 품질의 지원이 가능하다.
+```
